bda42035e223b1b494b1f8e957a067373e81b7ff
Tyuiu.AshirbakievAR@outlook.com <almazashirbakiev@gmail.com>
2023-10-06 15:21:28 +0500

    Add project files.

---
+++ Tyuiu.AshirbakievAR.Sprint1.Task0.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AshirbakievAR.Sprint1.Task0.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AshirbakievAR.Sprint1.Task0.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba07dc16-b949-418a-bbb9-7c34c401b51b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
